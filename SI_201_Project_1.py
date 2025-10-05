b8612fa36f530a1744c2ab98938a072206aa3553
Rohan Karunakaran <rohanvk@umich.edu>
2025-10-05 19:53:17 -0400

    Initial files

---
+++ SI_201_Project_1.py
@@ -0,0 +1,9 @@
+# SI 201 HW4
+# Your name: Rohan Karunakaran 
+# Your student id: 80717574
+# Your email: rohanvk@umich.edu
+# Who or what you worked with on this homework (including generative AI like ChatGPT): ChatGPT
+# Asked Chatgpt hints for debugging and suggesting the general sturcture of the code
+
+fhand = open('SampleSuperstore.csv')
+print(fhand)
